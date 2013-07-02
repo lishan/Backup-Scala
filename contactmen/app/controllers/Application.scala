4192a09ac6b314521e6024fdb733b8a1fb49fbf8
Fred <lishan.yu@oracle.com>
2013-07-03 01:38:01 +0800

    connect to database and run with bootstrap

--- contactmen/app/controllers/Application.scala
+++ contactmen/app/controllers/Application.scala
@@ -1,15 +1,18 @@
 package controllers
 
 import play.api.mvc._
+import play.api.db.DB
+import play.api.Play.current
+import anorm._
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    DB.withConnection{implicit c=>
+        val result = SQL("select * from people")().map(row => row[Int]("id") -> row[String]("name")).toList;
+        Ok(views.html.index(result(0)._2));
+    }
+
   }
 
-  def task = Action {
-    Ok(views.html.index("test message"))
-  }
-  
 }
